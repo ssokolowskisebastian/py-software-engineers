a4b985c20c77ab077148608ea6499d782536ef0c
Sebastian Sokolowski <wielewiak08@gmail.com>
2025-07-21 18:44:10 +0200

    Final Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,63 @@
-# write your code here
+class SoftwareEngineer:
+    def __init__(self, name: str, skills = []) -> None:
+        self.name = name
+        self.skills = skills
+
+    def learn_skill(self, skill: str) -> None:
+        self.skills.append(skill)
+
+
+class FrontendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str,
+                 skills = ["JavaScript", "HTML", "CSS"]) -> None:
+        super().__init__(skills)
+        self.skills = skills
+        self.name = name
+
+    def create_awesome_web_page(self) -> str:
+        print(f"{self.name} is creating a webpage...")
+        return "<h1>Hello world</h1>"
+
+
+class BackendDeveloper(SoftwareEngineer):
+    def __init__(self, name: str,
+                 skills = ["Python", "SQL", "Django"]) -> None:
+        super().__init__(skills)
+        self.skills = skills
+        self.name = name
+
+    def create_powerful_api(self) -> str:
+        print(f"{self.name} is creating an API...")
+        return "http://127.0.0.1:8000"
+
+
+class AndroidDeveloper(SoftwareEngineer):
+    def __init__(self, name: str,
+                 skills = ["Java", "Android studio"]) -> None:
+        super().__init__(skills)
+        self.skills = skills
+        self.name = name
+
+    def create_smooth_mobile_app(self) -> str:
+        print(f"{self.name} is creating a mobile app...")
+        return "Ads every three swipes"
+
+
+class FullStackDeveloper(FrontendDeveloper, BackendDeveloper):
+    def __init__(self, name: str,
+                 skills = ["Python", "SQL", "Django", "JavaScript", "HTML", "CSS"]) -> None:
+        super().__init__(skills)
+        self.skills = skills
+        self.name = name
+
+    def create_web_application(self) -> None:
+        print(f"{self.name} started creating a web application...")
+        self.create_powerful_api()
+        self.create_awesome_web_page()
+
+
+
+
+
+
+
