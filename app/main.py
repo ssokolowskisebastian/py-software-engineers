8b89695c552bdd20c6811a40883ecd42e9c887ca
Sebastian Sokolowski <wielewiak08@gmail.com>
2025-07-21 18:55:41 +0200

    skills inside the methods

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 class SoftwareEngineer:
-    def __init__(self, name: str, skills: list[str] = []) -> None:
+    def __init__(self, name: str, skills: list[str]) -> None:
         self.name = name
-        self.skills = skills
+        self.skills = []
 
     def learn_skill(self, skill: str) -> None:
         self.skills.append(skill)
